da8c019862e550c9346d18227f16b0c87eec5f89
ShwRat410 <shwetaratnawat410@gmail.com>
2025-03-18 15:15:54 +0000

    Showing error pages with errorElement

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
 
 const router = createBrowserRouter([
   {
     path:'/',
     element:<RootLayout></RootLayout>,
+    errorElement:<ErrorPage></ErrorPage>,
     children:[
       {path:'/',element:<HomePage></HomePage>},
       {path:'/products',element:<ProductsPage></ProductsPage>}
---
+++ src/pages/Error.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavigationPage from '../components/MainNavigation'
+
+export default function ErrorPage() {
+  return (
+    <div>
+        <NavigationPage></NavigationPage>
+        <p>An error occured</p>
+    </div>
+  )
+}
