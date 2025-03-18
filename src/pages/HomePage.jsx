957bb42db71721d9e8ea192c7640d7c051b4c93f
ShwRat410 <shwetaratnawat410@gmail.com>
2025-03-18 16:25:42 +0000

    Navigating programatically using useNavigate

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 export default function HomePage() {
+
+  const navigate = useNavigate()
+
+  function handleNavigate(){
+    navigate('/products')
+  }
+
   return (
     <div>
       <p>Home Page <Link to="/products">Link to product page</Link></p>
+      <button onClick={handleNavigate}>Navigate</button>
     </div>
   )
 }
